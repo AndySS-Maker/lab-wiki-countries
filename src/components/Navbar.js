36c71c50ad7ea1db9e7778d553e7f6afbd3f5ba8
andyss <andy_ss_22@icloud.com>
2021-06-27 04:15:47 -0300

    lab done

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+  return  (
+    <nav class="navbar navbar-dark bg-primary">
+      <span class="navbar-brand mb-0 h1">LAB - WikiCountries</span>
+    </nav>
+  )
+}
+
+export default Navbar;
